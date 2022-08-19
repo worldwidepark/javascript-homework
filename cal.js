545f1987129264dcaf672dae84f8d92747749773
worldwidepark <qkrguddn1066@naver.com>
2022-08-19 21:41:04 +0900

    js homework カレンダーコメント追加

--- cal.js
+++ cal.js
@@ -1,12 +1,13 @@
+//-m オプション
 const { Command } = require("commander");
 const program = new Command();
-
 program.option("-m, --month <month>").parse();
 
 let today = new Date();
 let year = String(today.getFullYear());
 let month;
 
+//-m オプションの値を評価
 if (1 <= program.opts().month && program.opts().month <= 12) {
   month = program.opts().month;
 } else if (!program.opts().month) {
@@ -24,16 +25,18 @@ let date = new Date(year, month - 1, 1).toLocaleString({
 let lastDate = new Date(year, month, 0).toLocaleString({
   timeZone: "Asia/Tokyo",
 });
-
+//初日の曜日
 let daySet = new Date(date).getDay();
 let lastDaySet = new Date(lastDate).getDate();
-
+//指定月が何週かを求める
 let weekCount = Math.ceil((daySet + lastDaySet) / 7);
+//2週目の初めての日にちを求める
 let firstDayOfWeek = 8 - daySet;
 let week = {};
+
 for (i = 1; i <= weekCount; i++) {
   week[i] = "";
-
+  //1週目
   if (i == 1) {
     for (j = 0; j < daySet; j++) {
       week[i] = week[i] + "   ";
@@ -42,6 +45,7 @@ for (i = 1; i <= weekCount; i++) {
       week[i] = week[i] + " " + n.toString().padStart(2);
     }
   } else if (i == weekCount) {
+    //最後の週
     for (j = 1; firstDayOfWeek <= lastDaySet; j++) {
       week[i] = week[i] + " " + firstDayOfWeek.toString().padStart(2);
       firstDayOfWeek++;
